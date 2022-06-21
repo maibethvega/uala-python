8ffcf47bd5bacc3635bd182c85ef321b84d4eea2
maibethvega <maibeth.vega@uala.com.ar>
2022-06-21 17:27:01 -0300

    pravtica 8

--- practica_clase_8.py
+++ practica_clase_8.py
@@ -65,10 +65,8 @@ for i in Alumno.alumnos:
 lista_de_promedios = [alum1.promedio, alum2.promedio, alum3.promedio, alum4.promedio, alum5.promedio]
 lista_de_notas = [alum1.mayor_nota, alum2.mayor_nota, alum3.mayor_nota, alum4.mayor_nota, alum5.mayor_nota]
 lista_de_nombres = [alum1.name, alum2.name, alum3.name, alum4.name, alum5.name]
-a = lista_de_promedios.index(estudianteMejorPromedio(lista_de_promedios))
-b = estudianteMejorPromedio(lista_de_promedios)
-c = lista_de_notas.index(estudianteMejorNota(lista_de_notas))
-d = estudianteMejorNota(lista_de_notas)
-print("+ El estudiante con Mejor Promedio es: " + lista_de_nombres[a] + ", con: " + str(b))
-print("+ El estudiante con Mejor Nota es: " + lista_de_nombres[c] + ", con: " + str(d))
+nombrePromedio = lista_de_promedios.index(estudianteMejorPromedio(lista_de_promedios))
+nombreNota = lista_de_notas.index(estudianteMejorNota(lista_de_notas))
+print("+ El estudiante con Mejor Promedio es: " + lista_de_nombres[nombrePromedio] + ", con: " + str(estudianteMejorPromedio(lista_de_promedios)))
+print("+ El estudiante con Mejor Nota es: " + lista_de_nombres[nombreNota] + ", con: " + str(estudianteMejorNota(lista_de_notas)))
 
